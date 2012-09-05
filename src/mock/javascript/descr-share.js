c1c00ba90aa18715b65ea66f32399645a609263d
jacky <jacky.wangzj@ef.com>
2012-09-05 12:03:28 +0800

    fix po bugs
    fix po bugs

--- src/mock/javascript/descr-share.js
+++ src/mock/javascript/descr-share.js
@@ -1,111 +1,128 @@
-$(function(){
-	//descr area valid logic
-	var checkTextLength = function(text , minLength){
-		if(typeof text === 'undefined'){
-			return false;
-		}
-		if(typeof minLength === "undefined"){
-			minLength = 0;
-		}
-		if(text.length > minLength){
-			return true;
-		}else{
-			return false;
-		}		
-	};
+$(function() {
 
-	var myDescribe;
-	var $textPlaceHolder = $('.ets-placeholder');
-	var validText = function(text){
-		var val = $.trim($describe.val());
-		if(checkTextLength(val)){
-			$textPlaceHolder.hide();
-			$describeArea.addClass('ets-valid');
-		}else{
-			$textPlaceHolder.show();
-			$describeArea.removeClass('ets-valid');
-		}
-		myDescribe = val;
-		$describeArea.trigger('change:tick');
-	};
-
-	var $describeArea = $('.ets-describe-area');
-	var $describe = $('#input-describe');
-	var defaultMaxTextLength = 250;
-	function initDescrArea(){
-		//init textarea
-		$describe.counter({
-        	goal: defaultMaxTextLength
-    	});
-
-		//bind valid text event handler
-		$describeArea.focusin(function(event) {
-			$textPlaceHolder.hide();
-		}).bind('input , focusout',function(e){			
-			validText();
-		});
-	}
-
-	//image area valid logic	
-    var previewWidth = 328;
-    var previewHeight = 292;
-    var $cropScreen = $('.ets-crop-area');
-	var $previewImage = $cropScreen.find('img');	
-	var prepareCropImg = function(imgurl){
-		$previewImage.attr("src", imgurl);
-		//fix default width not enough
-		$previewImage[0].onload = function(){
-			if($previewImage.width() < previewWidth 
-					&& $previewImage.height() < previewHeight){
-				$previewImage.css({
-					width : previewWidth,
-					height : previewHeight
-				});
-			}
-		};
-	};
-
-
-	var guideToCropImgArea = function(){
-		$galleryScreen.addClass('ets-none');
-    	$progressScreen.addClass('ets-none');
-    	$uploadBtn.addClass('ets-none');
-
-    	$cropScreen.removeClass('ets-none');
-    	$changeImageBtn.removeClass('ets-none');
-    
-    	$imageArea.addClass('ets-valid');
+	//Describe Area
+    var checkTextLength = function(text, minLength) {
+        if (typeof text === 'undefined') {
+            return false;
+        }
+        if (typeof minLength === "undefined") {
+            minLength = 0;
+        }
+        if (text.length > minLength) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+        
+    var $textPlaceHolder = $('.ets-placeholder');
+    var $describeArea = $('.ets-describe-area');
+    var $describe = $('#input-describe');
+    var validText = function(text) {
+        var val = $.trim($describe.val());
+        if (checkTextLength(val)) {
+            $textPlaceHolder.hide();
+            $describeArea.addClass('ets-valid');
+        } else {
+            $textPlaceHolder.show();
+            $describeArea.removeClass('ets-valid');
+        }        
+        $describeArea.trigger('change:tick');//TroopJs will use hub instead of this.
     };
 
-	var selectedImgUrl;
-	var $imageArea = $('.ets-select-image-area');
-	var $galleryScreen = $('.ets-gallery');
-	var $imgLib = $galleryScreen.find('li a');
-	var $progressScreen = $('.ets-progress-wrapper');
-	var $cropScreen = $('.ets-crop-area');
-	var $uploadBtn = $('#upload-image');
-	var $changeImageBtn = $('#change-image');
-	function initSelectImage(){
-		$imgLib.click(function(event) {
-			//prepare the img 
-			selectedImgUrl = $(this).find('img').attr('src');
-			prepareCropImg(selectedImgUrl);
-		
-			//guide to crop imag area 
-	        guideToCropImgArea();
-			
-			//trigger change:tick
-			$imageArea.trigger('change:tick');
+    var warmTextLength = 240;    
+    var warmingLackCharsLeft = function(){        
+        var $LeftCharNumSpan = $('#input-describe_count');    
+        var val = $.trim($describe.val());        
+        if(val.length >= warmTextLength){
+            console.log($LeftCharNumSpan);
+            $LeftCharNumSpan.addClass('warmColor');
+        }else{
+            $LeftCharNumSpan.removeClass('warmColor');
+        }
+    }
 
-			//block bubble and default handler
-			return false;
-		});
-	}
+    //Shared CheckBox Area
+    function sharedWithOthers()  {
 
-    var makeCropImgDragable = function(){
-		var imgWidth = $previewImage.width();
-        var imgHeight = $previewImage.height();
+        var $sharedCheckBoxArea = $describeArea.find('.share-info');        
+        $sharedCheckBoxArea.bind('hover', function (e) {
+            if (e.type === "mouseenter") {
+                $(this).addClass('share-info-hover');
+            } else if (e.type === "mouseleave") {
+                $(this).removeClass('share-info-hover');
+            }
+        });
+    }
 
+    var defaultMaxTextLength = 250;
+    function initDescrArea() {
+        //init textarea
+        $describe.counter({
+            goal: defaultMaxTextLength
+        });
+
+        //bind valid text event handler
+        $describeArea.focusin(function(event) {
+            $textPlaceHolder.hide();
+        }).bind('keyup , focusout', function(e) {
+            validText();
+            warmingLackCharsLeft();
+        });     
+
+        //Shared with others
+        sharedWithOthers();
+    }
+
+
+    //Image Area
+    var previewWidth = 329;
+    var previewHeight = 329;
+    var $cropScreen = $('.ets-crop-area');
+    var $previewImage = $cropScreen.find('img');
+    var prepareCropImg = function(imgurl) {
+        $previewImage.attr("src", imgurl);
+    };    
+    
+    var guideToCropImgArea = function() {
+        
+        $imageArea.addClass('ets-selected-image ets-valid');
+        $uploadBtn.addClass('ets-disabled');
+
+        //trigger change:tick
+        $imageArea.trigger('change:tick');
+    };   
+    
+    var selectedImgUrl;
+    var $imageArea = $('.ets-select-image-area');
+    var $galleryScreen = $('.ets-gallery');
+    var $imgLib = $galleryScreen.find('li a');
+    var $progressScreen = $('.ets-progress-wrapper');
+    var $cropScreen = $('.ets-crop-area');
+    var $uploadBtn = $('#upload-image');
+    var $changeImageBtn = $('#change-image');
+    function initSelectImage() { 
+        $imgLib.click(function(event) {            
+            selectedImgUrl = $(this).find('img').attr('src');
+            prepareCropImg(selectedImgUrl);     
+            guideToCropImgArea();
+
+            //fix current mock image to somall bug
+            $previewImage.css({
+                width: 298,
+                height: 298
+            });
+
+            //block bubble and default handler
+            return false;
+        });
+    }
+    
+    var makeCropImgDragable = function() {
+    	//use dom can get real width and height ,jQuery can't
+        var imgWidth = $previewImage[0].width;
+        var imgHeight = $previewImage[0].height;
+        
         if (imgWidth <= imgHeight) {
             $previewImage.css({
                 width: previewWidth,
@@ -117,12 +134,12 @@ $(function(){
                 height: previewHeight
             });
         }
-
+        
         var width = $previewImage.width() - previewWidth;
         var height = $previewImage.height() - previewHeight;
-
+        
         $previewImage.draggable({
-            drag: function (e, ui) {
+            drag: function(e, ui) {
                 if (ui.position.left >= 0) {
                     ui.position.left = 0;
                 }
@@ -135,143 +152,147 @@ $(function(){
                 if (ui.position.top <= -height) {
                     ui.position.top = -height;
                 }
+            },
+            cursor: "move"
+        });
+    }
+    
+    var getImageUrlByFile = function(dfd, file) {
+
+        //prototype : use html5 filereader		
+        var fileReader = new FileReader();
+        fileReader.onload = function(e) {
+            selectedImgUrl = e.target.result;
+            dfd.resolve();
+        };
+        fileReader.onerror = function(e) {
+        //action about error handler
+        };
+        fileReader.readAsDataURL(file);
+
+    //rtm : use upload server
+    //...
+    };
+    
+    var $file = $('input:file');
+    function initUploadBtn() {
+        $uploadBtn.click(function(e) {
+            if ($uploadBtn.hasClass('ets-disabled')) {
+                return;
+            }
+            $file.trigger('click');
+        });
+        
+        $file.change(function(e) {
+            var file = $file[0].files[0];
+            if (file) {
+                // getAsImage(file);            	
+                $.Deferred(function(d) {
+                    getImageUrlByFile(d, file);
+                }).done(function() {
+                    prepareCropImg(selectedImgUrl);
+                    makeCropImgDragable();
+                    guideToCropImgArea();
+                });            
             }
         });
     }
+    
+    var guideToSelectImgArea = function() {
+        $previewImage.attr("src", '');
+        $uploadBtn.removeClass('ets-disabled');
+        $imageArea.removeClass('ets-selected-image ets-valid');
+    };
+    
+    function initChangeBtn() {
+        $changeImageBtn.click(function(e) {
 
-	var getImageUrlByFile = function(file){
-		
-		//prototype : use html5 filereader
-		var dfd = $.Deferred();
-		var fileReader = new FileReader();		
-		fileReader.onload = function(e){
-			selectedImgUrl = e.target.result;
-			dfd.resolve();
-		};
-		fileReader.onerror = function(e){
-			//action about error handler
-		};
-		fileReader.readAsDataURL(file);
-		return dfd.promise();
+            //guide to select & upload image
+            guideToSelectImgArea();
 
+            //trigger change:tick
+            $imageArea.trigger('change:tick');
 
-		//rtm : use upload server
-		//...
-	};
+            //clean crop setting
+            $previewImage.draggable('destroy').css({
+                width: 'auto',
+                height: 'auto',
+                left: 0,
+                top: 0,
+                cursor: 'default'
+            });
+        });
+    }
 
-	var $file = $('input:file');
-	function initUploadBtn(){
-		$uploadBtn.click(function (e) {
-        	if ($uploadBtn.hasClass('ets-disabled')) {
-            	return;
-        	}
-        	$file.trigger('click');
-    	});
+    // function cropImageSetting(){    	
+    //     $previewImage[0].onload = function(e,dfd) {
+    //         makeCropImgDragable();          
+    //     };
+    // }
 
-    	$file.change(function (e) {
-        	var file = $file[0].files[0];
-        	if (file) {
-            	// getAsImage(file);            	
-            	$.when(getImageUrlByFile(file))
-            		.done(function (){
-            			prepareCropImg(selectedImgUrl);
-            			guideToCropImgArea();
-            			makeCropImgDragable();
-            			
-            			//trigger change:tick
-	        			$imageArea.trigger('change:tick');
-            		});
-            	
-        	}
-    	});
-	}
-
-	function initChangeBtn (){
-		$changeImageBtn.click(function (e) {
-			//guide to select & upload image
-	        $changeImageBtn.addClass('ets-none');
-	        $progressScreen.addClass('ets-none');
-	        $cropScreen.addClass('ets-none');
-
-	        $galleryScreen.removeClass('ets-none');
-	        $uploadBtn.removeClass('ets-none ets-disabled');
-	        $imageArea.removeClass('ets-valid');
-	        
-	        //trigger change:tick
-	        $imageArea.trigger('change:tick');
-
-	        //clean crop setting
-	        $previewImage.draggable('destroy').css({
-	            width: 'auto',
-	            height: 'auto',
-	            left: 0,
-	            top: 0,
-	            cursor: 'default'
-	        });
-    	});
-	}
-
-	//image wall control logic
-	var $editProfleBtn = $('#edit-profile');
-    function initImageEditBtn(){
-    	    $editProfleBtn.click(function (e) {
-		        //trigger eidt user discribe and image
-		        $('#next-step').trigger('change:edit');
-		    });
+    //image wall control logic
+    var $editProfleBtn = $('#edit-profile');
+    function initImageEditBtn() {
+        $editProfleBtn.click(function(e) {
+            //trigger eidt user discribe and image
+            $('#next-step').trigger('change:edit');
+        });
     }
 
 
-	//next button valid logic
-	var $nextBtn = $('#next-step')
-	var $wall = $('.ets-profile-wall');
-	function initNextBtn (){
-		//listen the custom event change:tick
-		$describeArea.add($imageArea).bind('change:tick',function(e){
-			if($describeArea.hasClass('ets-valid') && $imageArea.hasClass('ets-valid')){
-				$nextBtn.removeClass('ets-disabled');
-			}else{
-				$nextBtn.addClass('ets-disabled');
-			}
-		});
+    //next button valid logic
+    var $nextBtn = $('#next-step')
+    var $wall = $('.ets-profile-wall');
+    function initNextBtn() {
+        //listen the custom event change:tick
+        $describeArea.add($imageArea).bind('change:tick', function(e) {
+            if ($describeArea.hasClass('ets-valid') && $imageArea.hasClass('ets-valid')) {
+                $nextBtn.removeClass('ets-disabled');
+            } else {
+                $nextBtn.addClass('ets-disabled');
+            }
+        });
 
-		//bind next button logic
-		$nextBtn.click(function (e) {
-	        if ($(this).hasClass('ets-disabled')) {
-	            return;
-	        } else {
-	            if (!$wall.hasClass('ets-none')) {
-	                alert('Activity completed!')
-	            } else {
-	                goToProfileWall();
-	            }
-	            
-	        }
-	    })
-		.bind('change:edit',function(e){
-			$wall.addClass('ets-none');
-        	$imageArea.add($describeArea).removeClass('ets-none');
-		});
+        //bind next button logic
+        $nextBtn.click(function(e) {
+            if ($(this).hasClass('ets-disabled')) {
+                return;
+            } else {
+                if (!$wall.hasClass('ets-none')) {
+                    alert('Activity completed!')
+                } else {
+                    goToProfileWall();
+                }
+            
+            }
+        })
+        .bind('change:edit', function(e) {
+            $wall.addClass('ets-none');
+            $imageArea.add($describeArea).removeClass('ets-none');
+        });
+        
+        function goToProfileWall() {
+            $.Deferred(function(dfd) {
+                $('.ets-profile-me').children('a').find('img').attr('src', selectedImgUrl);
 
-	    function goToProfileWall () {
-	        $.Deferred(function (dfd) {
-	            $('.ets-profile-me').children('a').find('img').attr('src', selectedImgUrl);
-	            $('.ets-profile-me .ets-tooltip-content').children('p').eq(0).text(myDescribe);
+                var myDescribe = $.trim($describe.val());
+                $('.ets-profile-me .ets-tooltip-content').children('p').eq(0).text(myDescribe);
+                
+                dfd.resolve();
+            }).done(function() {
+                $wall.removeClass('ets-none');
+                $imageArea.add($describeArea).addClass('ets-none');
+            });
+        }
+    }
 
-	            dfd.resolve();
-	        }).done(function () {
-	            $wall.removeClass('ets-none');
-	            $imageArea.add($describeArea).addClass('ets-none');	            
-	        });
-    	}
-	}
-
-	//init 
-	!function(){
-		initDescrArea();
-		initSelectImage();
-		initUploadBtn();
-		initChangeBtn();
-		initNextBtn();
-		initImageEditBtn();
-	}();
+    //init 
+    !function() {
+        initDescrArea();        
+        initSelectImage();
+        initUploadBtn();
+        initChangeBtn();
+        initNextBtn();
+        initImageEditBtn();
+    }();
 })
--- src/mock/javascript/show-tooltip.js
+++ src/mock/javascript/show-tooltip.js
@@ -14,5 +14,39 @@ $(function () {
             $parent.toggleClass('ets-on');
         }
     });
+
+    //control the add friend action 
+    function addFriendTrigger(me , text ){
+        $(me).addClass('ets-disabled');
+        $(me).find('span').text(text);
+        //soem ajax request add friend
+    }
+
+    //mock some friends and some waiting conform request
+    function mockSomeFriends(relationShip){
+        var random = Math.random();        
+        if(random < 0.3){ //friends            
+            addFriendTrigger(relationShip , alreadyFriend);
+        }else if(random > 0.8){//waiting
+            addFriendTrigger(relationShip , messageSent);
+        }
+    }
+
+    var alreadyFriend = 'Your are friends';
+    var messageSent = 'Friend request sent';
+    //bind Add friend event
+    $li.not('.ets-profile-me').find('.ets-tooltip .ets-tooltip-btns span.ets-btn-small').each(function(event) {
+
+        mockSomeFriends(this);
+
+        var me = $(this);
+        if(!me.hasClass('ets-disabled')){
+           me.click(function(){   
+                addFriendTrigger(me , messageSent);
+                return false;
+            });
+        }
+    });
+
     
 });
