e29e07f25164cddf6cefe6a9732cb2f10b40d2a0
Ika.Wu <wunianzu515@gmail.com>
2012-09-10 15:09:19 +0800

    Updated the template.

--- src/js/widget/share-and-describe/main.js
+++ src/js/widget/share-and-describe/main.js
@@ -1,25 +1,51 @@
 define(['compose',
+    'jquery',
     'troopjs-core/component/widget',
     'troopjs-utils/deferred',
-    'template!./main.html'], function shareAndDescribe(Compose, Widget, Deferred, template) {
+    'template!./main.html'], function shareAndDescribe(Compose, $, Widget, Deferred, template) {
     "use strict";
+
+    var mockData = {
+        content: {
+            imageLib: [
+                {url: 'mock/image/defaultOptions/chinese_s.jpg'},
+                {url: 'mock/image/defaultOptions/hamburger_s.jpg'},
+                {url: 'mock/image/defaultOptions/frenchfries_s.jpg'},
+                {url: 'mock/image/defaultOptions/pasta_s.jpg'},
+                {url: 'mock/image/defaultOptions/pizza_s.jpg'},
+                {url: 'mock/image/defaultOptions/sallad_s.jpg'},
+                {url: 'mock/image/defaultOptions/salmon_s.jpg'},
+                {url: 'mock/image/defaultOptions/seafood_s.jpg'},
+                {url: 'mock/image/defaultOptions/sushi_s.jpg'},
+                {url: 'mock/image/defaultOptions/toast_s.jpg'}
+            ]
+        }
+        
+    };
+
 
     // fitler origin _json data to meet activity requirements
     function filterData() {
 
     }
 
+    // render template and insert it into html
     function render(deferred) {
         var me = this;
+        var $root = this.$element;
+
         Deferred(function (renderDfd) {
             me.html(template, me._json, renderDfd);
         }).done(function () {
+            me.$placehodler = $root.find('.ets-placeholder');
             deferred.resolve();
         });
     }
 
     return Widget.extend(function () {
-        this._json = {};
+        this._json = mockData;
+
+
     }, {
         'sig/initialize': function onStart(signal, deferred) {
             var me = this;
@@ -30,6 +56,36 @@ define(['compose',
             }).done(function () {
                 render.call(me, deferred);
             });
+        },
+
+        // dom interaction
+        'dom/action.click.focusin.focusout.mouseout.mouseover': $.noop,
+        'dom/action/describe.focusin': function (topic, $e, index) {
+            var me = this;
+            var $target = $($e.target);
+
+            me.$placehodler.addClass('ets-none');
+        },
+        'dom/action/describe.focusout': function (topic, $e, index) {
+            var me = this;
+            var $target = $($e.target);
+
+            if (!$.trim($target.val()).length) {
+                me.$placehodler.removeClass('ets-none');
+            }
+        },
+        'dom/action/agree.mouseover': function (topic, $e, index) {
+            var me = this;
+            var $target = $($e.target);
+
+            $target.parent().addClass('share-info-hover');
+        },
+        'dom/action/agree.mouseout': function (topic, $e, index) {
+            var me = this;
+            var $target = $($e.target);
+
+            $target.parent().removeClass('share-info-hover');
         }
+
     })
 });
