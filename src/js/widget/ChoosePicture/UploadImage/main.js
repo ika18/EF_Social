8e8da624b0681a69929580c111d0d30a70c21358
qiang.zhang <qiang.zhang@ef.com>
2012-10-10 15:43:21 +0800

    Make a draft for troop

---
+++ src/js/widget/ChoosePicture/UploadImage/main.js
@@ -0,0 +1,18 @@
+define([
+    "jquery",
+    'troopjs-core/component/widget',
+    'template!./main.html',
+    'troopjs-utils/deferred'
+], function DemoModule($, Widget, template, Deferred) {
+	"use strict";
+
+	return Widget.extend({
+		"sig/start": function(signal, deferred) {
+			var me=this;
+			me.html(template);
+			alert("sdf");
+		}
+
+	});
+
+});
